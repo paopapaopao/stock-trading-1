8774e49c1a1c908d795891ffa464a68944c67411
paopapaopao <paolo.casugay@gmail.com>
2021-08-16 22:58:53 +0800

    Add: show all users system spec

---
+++ spec/system/show_all_users_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'Show All Users', type: :system do
+  before(:all) { driven_by :rack_test }
+
+  context 'Show all users' do
+    it do
+      visit users_path
+      expect(page).to have_content 'Users'
+      expect(page).to have_current_path users_path
+    end
+  end
+end
