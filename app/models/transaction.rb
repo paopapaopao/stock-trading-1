81b6ea9ac16e3522227a926b0a4ddb25cf502322
paopapaopao <paolo.casugay@gmail.com>
2021-08-17 00:46:14 +0800

    Add: value attribute to transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,6 @@
 class Transaction < ApplicationRecord
   validates :transaction_type,
     presence: true, inclusion: { in: %w(buy sell) }
+  validates :value,
+    presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
