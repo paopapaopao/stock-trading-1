34b43b360fe47e6a0a41ad5f1c014014170f231b
paopapaopao <paolo.casugay@gmail.com>
2021-08-16 23:54:35 +0800

    Add: transaction type column to transactions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_154833) do
+ActiveRecord::Schema.define(version: 2021_08_16_155154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_08_16_154833) do
   create_table "transactions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "transaction_type"
   end
 
   create_table "users", force: :cascade do |t|
