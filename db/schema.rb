625e6b0aebcb2e455e3929d07ca748b87bfed518
paopapaopao <paolo.casugay@gmail.com>
2021-08-17 00:42:48 +0800

    Add: user reference column to transactions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_155727) do
+ActiveRecord::Schema.define(version: 2021_08_16_164036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,8 @@ ActiveRecord::Schema.define(version: 2021_08_16_155727) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "transaction_type"
     t.decimal "value"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_transactions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_08_16_155727) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "transactions", "users"
 end
