22d6f498bbdc784f3eb84bf3b043251ea38c3b75
paopapaopao <paolo.casugay@gmail.com>
2021-08-17 00:52:19 +0800

    Add: create roles migration file and role model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_164036) do
+ActiveRecord::Schema.define(version: 2021_08_16_165117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "roles", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string "company_name"
