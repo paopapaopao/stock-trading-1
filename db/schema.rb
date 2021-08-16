e1d2509f1f6bcdbf0ca172672e8734002cdfaab7
paopapaopao <paolo.casugay@gmail.com>
2021-08-17 01:01:02 +0800

    Add: create user roles migration and user role model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_165331) do
+ActiveRecord::Schema.define(version: 2021_08_16_165935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 2021_08_16_165331) do
     t.decimal "value"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
+
+  create_table "user_roles", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
