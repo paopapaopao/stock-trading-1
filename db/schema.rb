c813dcb24757f46f72be486c1cdd9dfec363fc19
paopapaopao <paolo.casugay@gmail.com>
2021-08-16 23:58:35 +0800

    Add: value column to transactions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_155154) do
+ActiveRecord::Schema.define(version: 2021_08_16_155727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_08_16_155154) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "transaction_type"
+    t.decimal "value"
   end
 
   create_table "users", force: :cascade do |t|
