8b21e9788c5e26db8cda9a2e6a214ecf4cfe8ee3
paopapaopao <paolo.casugay@gmail.com>
2021-08-17 00:54:01 +0800

    Add: name column to roles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_165117) do
+ActiveRecord::Schema.define(version: 2021_08_16_165331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_08_16_165117) do
   create_table "roles", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "stocks", force: :cascade do |t|
