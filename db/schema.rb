b6c0cfde3c0eb465d1f7054502169d4ecdc2b214
paopapaopao <paolo.casugay@gmail.com>
2021-08-16 23:49:44 +0800

    Add: transaction model and transactions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_16_121745) do
+ActiveRecord::Schema.define(version: 2021_08_16_154833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 2021_08_16_121745) do
     t.string "company_name"
     t.integer "price"
     t.integer "market_cap"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "transactions", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
