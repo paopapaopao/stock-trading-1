6c5155f0a77264001799c971bc8701ac1fbf673b
paopapaopao <paolo.casugay@gmail.com>
2021-08-16 20:21:37 +0800

    Add: user_type attribute to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :user_type,
+    presence: true, inclusion: { in: %w(buyer broker buyer_and_broker) }
 end
