58763b9893830100f65bf7cc0a7bd70baedc0f3a
paopapaopao <paolo.casugay@gmail.com>
2021-08-27 15:00:34 +0800

    Setup the email devise will use to send emails

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 465,
+  domain:               'gmail.com',
+  # email address devise will use to send emails (e.g. 'eadd@example.com')
+  user_name:            '',
+  # password of the email address (e.g. 'password')
+  password:             '',
+  authentication:       :plain,
+  ssl:                  true,
+  tsl:                  true,
+  enable_starttls_auto: true
+}
