2066f656efc6016a4a0c9eaf29f5aff7899f76bf
paopapaopao <paolo.casugay@gmail.com>
2021-08-27 14:57:58 +0800

    Add the confirmable module from devise to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
 
   # validates :role,
   #   presence: true, inclusion: { in: %w(buyer broker buyer_and_broker) }
